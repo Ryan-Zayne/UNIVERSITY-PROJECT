0190a0f82e2a1c7ce8076dac2fc772d1aea33298
Ryan-Zayne <phoenixastro97@gmail.com>
2025-06-30 00:36:24 +0100

    .

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,14 @@
 import tailwindcss from "@tailwindcss/vite";
-import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 import viteTsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-	plugins: [viteTsconfigPaths(), TanStackRouterVite({ autoCodeSplitting: true }), react(), tailwindcss()],
+	plugins: [
+		viteTsconfigPaths(),
+		tanstackRouter({ autoCodeSplitting: true, indexToken: "page" }),
+		react(),
+		tailwindcss(),
+	],
 });
